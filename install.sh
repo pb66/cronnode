d5788c9affc7c52214c4ef600b5229c158181162
pb66 <pb66@outlook.com>
2014-05-07 13:58:34 +0000

    made executable

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ echo "Installing CronNode for emonHub................"
 sudo mkdir $INST_PATH
 sudo mv cronnode.py $INST_PATH
 sudo ln usr/bin/cronnode $INST_PATH/cornnode.py
-sudo chmod =x $INST_PATH/cornnode.py
+#sudo chmod =x $INST_PATH/cornnode.py
 sudo rm -r $INST_PATH
 echo "run `crontab -e` to edit cron tab"
 echo "example entry"
