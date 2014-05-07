b948d1df0d066ee5269ea19c76eb26d7c22f32d2
Paul <pb66@users.noreply.github.com>
2014-05-07 15:03:23 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo mv cronnode.py $INST_PATH
 sudo ln usr/bin/cronnode $INST_PATH/cornnode.py
 #sudo chmod =x $INST_PATH/cornnode.py
 sudo rm -r $INST_PATH
-echo "run `crontab -e` to edit cron tab"
+echo "run ' crontab -e ' to edit cron tab"
 echo "example entry"
 echo "#### GMT settings ####
 echo "#15 00 * * * cronnode offPeak
