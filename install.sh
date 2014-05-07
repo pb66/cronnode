751565075f8cb57f4b0d6b023ddd9b24d90fcd5f
Paul <pb66@users.noreply.github.com>
2014-05-07 14:52:23 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 INST_PATH=/usr/share/emonhub/cronnode
-TEMP_PATH=`pwd`/cronnode
 echo
 echo "Installing CronNode for emonHub................"
 sudo mkdir $INST_PATH
 sudo mv cronnode.py $INST_PATH
 sudo ln usr/bin/cronnode $INST_PATH/cornnode.py
+sudo chmod =x $INST_PATH/cornnode.py
+sudo rm -r $INST_PATH
 echo "run `crontab -e` to edit cron tab"
 echo "example entry"
 echo "#### GMT settings ####
-echo "#15 00 * * * cron_node offPeak
-echo "#15 07 * * * cron_node onPeak
+echo "#15 00 * * * cronnode offPeak
+echo "#15 07 * * * cronnode onPeak
 echo "#### BST settings ####
-echo "15 01 * * * cron_node offPeak
-echo "15 08 * * * cron_node onPeak
+echo "15 01 * * * cronnode offPeak
+echo "15 08 * * * cronnode onPeak
 
 
 
